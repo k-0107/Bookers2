099929f252df39e500067dcf652dcc53c1e1b210
EC2 Default User <ec2-user@ip-172-31-47-110.ap-northeast-1.compute.internal>
2024-01-09 02:07:17 +0000

    [Finish]エラー実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-
+before_action :is_matching_login_user, only: [:edit, :update]
   def index
     @books = Book.all
     @book = Book.new
@@ -8,8 +8,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "successfully"
+    else
+      @books = Book.all
+      render "books/index"
+    end
   end
 
   def show
@@ -22,8 +27,12 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to(book_path(@book.id))
+      flash[:notice] ="Book was successfully updated."
+    else
+      render "books/edit"
+    end
   end
   
   def destroy
@@ -33,9 +42,16 @@ class BooksController < ApplicationController
   end
   
    private
+   
    def book_params
      params.require(:book).permit(:title,:body)
    end
-  
+   
+   def is_matching_login_user
+     book = Book.find(params[:id])
+     unless book.user_id == current_user.id
+       redirect_to books_path
+     end
+   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :name,  length: { minimum: 2, maximum: 140 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+         
   has_many :books, dependent: :destroy
   
   has_one_attached :profile_image
